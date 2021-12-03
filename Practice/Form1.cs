a7e117ff01f4197f635d7c1a545d598b3abf06c6
Kosov Andrey <andrewkosov13@gmail.com>
2021-12-03 17:02:34 +0300

    Добавлено задание 2_2

--- Practice/Form1.cs
+++ Practice/Form1.cs
@@ -16,5 +16,57 @@ namespace Practice
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле должно содержать цифры");
+            }
+        }
+
+        private void TimeConsumingMethod(int seconds)
+        {
+            for (int j = 1; j <= seconds; j++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                SetProgress((int)(j * 100) / seconds);
+                if (Cancel)
+                    break;
+                if (Cancel)
+                {
+                    System.Windows.Forms.MessageBox.Show("Cancelled");
+                    Cancel = false;
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Complete");
+                }
+            }
+        }
+
+        private delegate void TimeConsumingMethodEventHandler(int seconds);
+
+        public delegate void SetProgressDelegate(int val);
+        public void SetProgress(int val)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+            else
+            {
+                progressBar1.Value = val;
+            }
+        }
+        bool Cancel;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeConsumingMethodDelegate del = new TimeConsumingMethodDelegate(TimeConsumingMethod);
+            del.BeginInvoke(int.Parse(textBox1.Text), null, null);
+        }
     }
 }
